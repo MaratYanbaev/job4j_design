d3f670608f689e80281519ccb7a691bc8e1b4909
Marat <marat.yanbaev@ardecs.com>
2021-05-02 02:18:19 +0400

    MatrixItTest

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -5,6 +5,9 @@ package ru.job4j;
  */
 public class App {
     public static void main(String[] args) {
+        int[][] in = {
+            {1}, {}, {}, {}, {2}
+        };
         System.out.println("Hello World!");
     }
 }
---
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -0,0 +1,44 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Marat Yanbaev
+ * @create 02.05.2021
+ */
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        for (; row < data.length; row++) {
+            if (data[row].length > column) {
+                return true;
+            } else {
+                column = 0;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        for (; row < data.length; row++) {
+            if (data[row].length > column) {
+                return data[row][column++];
+            }
+            column = 0;
+        }
+        return null;
+    }
+}
