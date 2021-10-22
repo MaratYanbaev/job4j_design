c651e764dd8bffc7550ccce68a3d46861e1510d3
Marat <marat.yanbaev@ardecs.com>
2021-10-22 23:44:01 +0400

    SimpleArrayList

---
+++ src/main/java/ru/job4j/list/List.java
@@ -0,0 +1,13 @@
+package ru.job4j.list;
+
+/**
+ * @author Marat
+ * @create 20.10.2021
+ */
+public interface List<T> extends Iterable<T> {
+    void add(T value);
+    T set(int index, T newValue);
+    T remove(int index);
+    T get(int index);
+    int size();
+}
---
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,94 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+/**
+ * @author Marat
+ * @create 20.10.2021
+ */
+public class SimpleArrayList<T> implements List<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        modCount++;
+        if (size == container.length) {
+            grow();
+        }
+        container[size] = value;
+        size++;
+    }
+
+    private void grow() {
+        container = Arrays.copyOf(container, container.length * 2);
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, container.length);
+        T oldValue = container[index];
+        container[index] = newValue;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, container.length);
+        T element = container[index];
+        if (--size > index) {
+            System.arraycopy(container, index + 1, container, index, size - index);
+        }
+        container[size] = null;
+        return element;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, container.length);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            int cursor;
+            int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (cursor < size) {
+                    return container[cursor++];
+                }
+                throw new NoSuchElementException();
+            }
+
+        };
+    }
+}
